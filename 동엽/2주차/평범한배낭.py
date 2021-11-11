e3be20e5666d43d746b6a84bab1fe3e1f315b9da
dongyeopca <ckehdduq95@gmail.com>
2021-11-11 13:19:13 +0900

    2주차dp

---
+++ 동엽/2주차/평범한배낭.py
@@ -0,0 +1,8 @@
+n,k = map(int,input().split())
+dp = [0]*(k+1)
+for _ in range(n):
+    w,v = map(int,input().split())
+    for j in range(k,w-1,-1):
+        dp[j] = max(dp[j-w]+v,dp[j])
+
+print(dp[k])
